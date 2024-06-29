a8fabe2896ab861a11605b6126dd7ab1ddec5e7c
Dark <evancavaraujo3@gmail.com>
2024-06-29 08:21:53 -0300

    1

--- workout app/src/App.jsx
+++ workout app/src/App.jsx
@@ -5,23 +5,22 @@ import React, { useEffect, useRef, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
-import Home from "./pages/home"
+import Home from "./pages/home";
 import Obesity from "./pages/obesity";
 import Lean from "./pages/lean";
 import Normal from "./pages/normal";
-
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="" element={<Home/>} />
+        <Route path="" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/obesity" element={<Obesity/>} />
+        <Route path="/obesity" element={<Obesity />} />
 
-        <Route path="/lean" element={<Lean/>} />
-        <Route path="/normal" element={<Normal/>} />
+        <Route path="/lean" element={<Lean />} />
+        <Route path="/normal" element={<Normal />} />
       </Routes>
     </Router>
   );
