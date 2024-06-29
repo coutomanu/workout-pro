52414f693402bfd2e6b1de39fb44726c58bb85e7
Paulo Henrique Motta <motta.phenrique@gmail.com>
2024-06-28 21:44:32 -0300

    paulinho

--- workout app/src/App.jsx
+++ workout app/src/App.jsx
@@ -3,14 +3,17 @@ import React, { useEffect, useRef, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
+import Home from "./pages/home"
+import Obesity from "./pages/obesity";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="" element={<Login />} />
+        <Route path="" element={<Home/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/obesity" element={<Obesity/>} />
       </Routes>
     </Router>
   );
